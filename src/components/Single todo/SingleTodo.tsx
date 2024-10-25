135efaa46373bb8831dc8613e7f63cdf48cfdc8c
ornops <joshuaornopia@gmail.com>
2024-10-25 22:17:07 +0800

    added components for single lists

--- src/components/Single todo/SingleTodo.tsx
+++ src/components/Single todo/SingleTodo.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './single-todo-styles.scss'
 import { Todo } from '../../model';
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+
 
 interface Props {
 	todo: Todo;
@@ -10,10 +13,24 @@ interface Props {
 }
 
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
-	return (
-		<div>
 
-		</div>
+	const handleDone = (id: number) => {
+		setTodos(todos.map((todo) =>
+			todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+		))
+	}
+
+	return (
+		<form action="" className='todos__single'>
+			{todo.isDone ? (<s className='todos__single--text'>{todo.todo}</s>) : (<span className='todos__single--text'>{todo.todo}</span>)
+			}
+
+			<div>
+				<span className='icon'><AiFillEdit /></span>
+				<span className='icon'><AiFillDelete /></span>
+				<span className='icon' onClick={() => handleDone(todo.id)}><MdDone /></span>
+			</div>
+		</form>
 	)
 }
 
